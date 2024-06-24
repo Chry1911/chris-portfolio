71d3f37257422afd272c028afa43455ca0689eea
= <=>
2024-06-25 00:06:12 +0200

    fix website action

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  basePath: '/chris-portfolio',
-  assetPrefix: '/chris-portfolio/',
-  images: {
-    unoptimized: true,
-  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
