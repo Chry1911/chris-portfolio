976d0f862e07ce3eab9e51ef0cc0f8d89dcb14b1
= <=>
2024-06-24 17:33:48 +0200

    changed config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   basePath: '/chris-portfolio',
   assetPrefix: '/chris-portfolio/',
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
